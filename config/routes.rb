304cde477e0401cd81c9eca489f22190680bd4f0
wimdavies <william.johnston.davies@gmail.com>
2023-11-14 18:54:14 +0000

    Add cover image upload to blog posts. turbo_stream allows image deletion without losing other changes while editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
-
-  resources :blog_posts
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
