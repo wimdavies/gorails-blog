7d07c3983d7294c012b06ccf69007115065fbcca
wimdavies <william.johnston.davies@gmail.com>
2023-11-14 15:35:53 +0000

    Add pagination using `pagy`

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,8 @@ user.update!(
   password: "password",
   password_confirmation: "password"
 )
+
+100.times do |index|
+  blog_post = BlogPost.where(title: "Blog Post ##{index}").first_or_initialize
+  blog_post.update(content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam laoreet convallis mauris, a imperdiet nibh condimentum in.", published_at: Time.current)
+end
