7531adb2092b7ffdd6b4c5874b7e3a9f8e3ab3ba
wimdavies <william.johnston.davies@gmail.com>
2023-11-14 20:26:37 +0000

    Configure action_mailer.default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'gorails-blog-87r4.onrender.com/', protocol: "https" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
